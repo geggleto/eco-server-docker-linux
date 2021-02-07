62d6dd7d2b8d419a0bd01fa83c7e2b2539285c89
PeeK1e <38333220+PeeK1e@users.noreply.github.com>
2021-02-07 05:01:42 +0100

    Expose UDP and TCP server port

--- start-container.sh
+++ start-container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "starting container..."
-echo $(docker run -d -v $2:/eco/Configs -v $3:/eco/Storage -p 3000:3000/udp -p 3000:3000/tcp -p 3001:3001 --restart=unless-stopped --cpus=2 $1)
+echo $(docker run -d -v $2:/eco/Configs -v $3:/eco/Storage -p 3000:3000/udp -p 3000:3000/tcp -p 3001:3001 --restart=unless-stopped $1)
